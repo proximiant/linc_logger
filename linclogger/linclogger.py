19b4682592c3b9408086b2013eb1848ed10efbba
Moonyoung Kang <yerihyo@gmail.com>
2019-09-25 13:11:08 -0700

    version bump

--- linclogger/linclogger.py
+++ linclogger/linclogger.py
@@ -3,7 +3,7 @@ import os
 from cloghandler import ConcurrentRotatingFileHandler
 from logging import WARNING as WARNING_LEVEL
 
-__version__ = '0.0.5'
+__version__ = '0.0.8'
 
 FILTERED_MODULES = [
     'kafka',
